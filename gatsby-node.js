80f6df35c8e679815e9311b2089690d94049c39e
yhl234 <200392342@student.georgianc.on.ca>
2020-04-25 13:53:36 -0400

    pagination

--- gatsby-node.js
+++ gatsby-node.js
@@ -11,6 +11,13 @@ exports.createPages = async ({ graphql, actions }) => {
           }
         }
       }
+      posts: allContentfulPost {
+        edges {
+          node {
+            slug
+          }
+        }
+      }
     }
   `);
 
@@ -20,6 +27,34 @@ exports.createPages = async ({ graphql, actions }) => {
       component: path.resolve('./src/templates/tour-template.js'),
       context: {
         slug: node.slug,
+      },
+    });
+  });
+  data.posts.edges.forEach(({ node }) => {
+    createPage({
+      path: `blog/${node.slug}`,
+      component: path.resolve('./src/templates/blog-template.js'),
+      context: {
+        slug: node.slug,
+      },
+    });
+  });
+  // amount of post
+  const posts = data.posts.edges;
+  // posts per page
+  const postsPerPage = 5;
+  // how many pages
+  const numPages = Math.ceil(posts.length / postsPerPage);
+  const pages = Array.from({ length: numPages });
+  pages.forEach((_, index) => {
+    createPage({
+      path: index === 0 ? '/blogs' : `/blogs/${index + 1}`,
+      component: path.resolve('./src/templates/blogs-list-template.js'),
+      context: {
+        limit: postsPerPage,
+        skip: index * postsPerPage,
+        numPages,
+        currentPage: index + 1,
       },
     });
   });
---
+++ src/templates/blogs-list-template.js
@@ -0,0 +1,74 @@
+import React from 'react';
+import { graphql } from 'gatsby';
+import Layout from '../components/Layout';
+import styles from '../css/blog.module.css';
+import AniLink from 'gatsby-plugin-transition-link/AniLink';
+import BlogCard from '../components/Blog/BlogCard';
+import Title from '../components/Title';
+
+const blogsTemplate = props => {
+  const { currentPage, numPages } = props.pageContext;
+  const isFirst = currentPage === 1;
+  const isLast = currentPage === numPages;
+  const nextPage = `/blogs/${currentPage + 1}`;
+  const prevPage = currentPage - 1 === 1 ? `/blogs` : `/blogs/${currentPage - 1}`;
+
+  const { data } = props;
+  return (
+    <Layout>
+      <section className={styles.blog}>
+        <Title title="latest" subtitle="posts" />
+        <div className={styles.center}>
+          {data.posts.edges.map(({ node }) => {
+            return <BlogCard key={node.index} blog={node} />;
+          })}
+        </div>
+        <section className={styles.links}>
+          {!isFirst && (
+            <AniLink fade to={prevPage} className={styles.link}>
+              prev
+            </AniLink>
+          )}
+
+          {Array.from({ length: numPages }, (_, i) => {
+            return (
+              <AniLink
+                fade
+                key={i}
+                to={`/blogs/${i === 0 ? '' : i + 1}`}
+                className={i + 1 === currentPage ? `${styles.link} ${styles.active}` : `${styles.link} `}
+              >
+                {i + 1}
+              </AniLink>
+            );
+          })}
+          {!isLast && (
+            <AniLink fade to={nextPage} className={styles.link}>
+              next
+            </AniLink>
+          )}
+        </section>
+      </section>
+    </Layout>
+  );
+};
+export const query = graphql`
+  query getPosts($skip: Int!, $limit: Int!) {
+    posts: allContentfulPost(skip: $skip, limit: $limit, sort: { fields: published, order: DESC }) {
+      edges {
+        node {
+          slug
+          title
+          id: contentful_id
+          published(formatString: "MMMM Do, YYYY")
+          image {
+            fluid {
+              ...GatsbyContentfulFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+export default blogsTemplate;
